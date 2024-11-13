eac834bc70f39f3300926907f9e2473cc8dac6a8
Lojain Abdalrazaq <lojain.jalal@gmail.com>
2024-11-13 21:48:35 +0200

    adding the server file

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+//import { logger } from './middlewares/loggerMiddleware';
+//import userRoutes from './routes/userRoutes';
+import sequelize from './config/database';
+import './models/SyncDB';
+
+// express application
+const app = express();
+const PORT = 3000;
+
+// Middlewares
+//app.use(logger); 
+//app.use(express.json());
+
+// Routes
+//app.use('/api/users', userRoutes);
+
+// starting the server
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`);
+});
