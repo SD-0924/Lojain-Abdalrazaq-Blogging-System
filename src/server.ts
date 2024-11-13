6615c247f1fe5c7b94985416b4f3399fb23a67e9
Lojain Abdalrazaq <lojain.jalal@gmail.com>
2024-11-13 21:49:56 +0200

    comment the sync import since the tables are created

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,19 @@
 import express from 'express';
-//import { logger } from './middlewares/loggerMiddleware';
-//import userRoutes from './routes/userRoutes';
-import sequelize from './config/database';
-import './models/SyncDB';
+import { logger } from './middlewares/loggerMiddleware';
+import userRoutes from './routes/userRoutes';
+/*import sequelize from './config/database';
+import './models/SyncDB';*/
 
 // express application
 const app = express();
 const PORT = 3000;
 
 // Middlewares
-//app.use(logger); 
-//app.use(express.json());
+app.use(logger); 
+app.use(express.json());
 
 // Routes
-//app.use('/api/users', userRoutes);
+app.use('/api/users', userRoutes);
 
 // starting the server
 app.listen(PORT, () => {
