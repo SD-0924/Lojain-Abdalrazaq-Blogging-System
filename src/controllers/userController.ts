9eca7b4fbc9ef7f163df2730465c491a2319da4a
Lojain Abdalrazaq <lojain.jalal@gmail.com>
2024-11-14 11:34:09 +0200

    removing update from validator to start step by step

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,10 +1,10 @@
 import { Request, Response } from 'express';
 import { handleError } from '../utils/errorHandler'; 
 import UserService from '../services/userService';  
-import { validateCreateUser, validateUpdateUser } from '../validations/userValidation';
+import { validateCreateUser } from '../validations/userValidation';
 
 // create a new user endpoint
-const createUser = async (req: Request, res: Response) => {
+const createUser = async(req: Request, res: Response) => {
     try {
         // validate input data
         const { error } = validateCreateUser(req.body);
@@ -18,5 +18,7 @@ const createUser = async (req: Request, res: Response) => {
         return handleError(req, res, 'Error creating user', 500);
     }
 };
+
+// delete a user endpoint
 
 export { createUser };
--- src/validations/userValidation.ts
+++ src/validations/userValidation.ts
@@ -6,20 +6,9 @@ const userValidationSchema = Joi.object({
     email: Joi.string().email().required(),
     password: Joi.string().min(6).required(),
 });
-
-const updateUserValidationSchema = Joi.object({
-    userName: Joi.string().min(3).max(50),
-    email: Joi.string().email(),
-    password: Joi.string().min(6),
-});
-
 // function checks if the input matches the schema for creating a user or updating a user
 const validateCreateUser = (userData: any) => {
     return userValidationSchema.validate(userData);
 };
 
-const validateUpdateUser = (userData: any) => {
-    return updateUserValidationSchema.validate(userData);
-};
-
-export { validateCreateUser, validateUpdateUser };
+export { validateCreateUser };
