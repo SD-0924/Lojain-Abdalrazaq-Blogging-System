07398c4a863b0986d6fd5136aaee90c0af77ea06
Lojain Abdalrazaq <lojain.jalal@gmail.com>
2024-11-14 22:36:36 +0200

    adding the service - repo - and create post endpoint

---
+++ src/controllers/postController.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from 'express';
+import { handleError } from '../utils/errorHandler'; 
+import postService from 'services/postService';
+import * as postValidator from 'validations/postValidation';
+
+// create a new post endpoint
+const createPost = async(req: Request, res: Response) => {
+
+    try{
+        // firstly, i have to validate the input data
+        const { error } = postValidator.validateCreatePost(req.body);
+        if(error){
+            return handleError(req, res, error.details[0].message, 422);
+        }
+
+        // call the service to create a post
+        const post = await postService.createPost(req.body);
+        if(!post){
+            return handleError(req, res, `The user with id ${req.body.id} is not exists` , 500);
+        }
+
+        // return the response with the created post
+        return res.status(201).json({ success: true, data: post });
+
+    }catch(err){
+        return handleError(req, res, 'Error in creating post', 500);
+    }
+
+};
---
+++ src/validations/postValidation.ts
@@ -0,0 +1,23 @@
+import Joi from 'joi';
+
+// * Schema Implementation * //
+const postValidationSchema = Joi.object({
+    title: Joi.string().min(3).max(50).required(),
+    content: Joi.string().min(6).required(),
+    userId: Joi.number().integer().positive().required(),
+});
+
+
+
+
+
+
+// * Functions Implementation * //
+const validateCreatePost = (postData: any) => {
+    return postValidationSchema.validate(postData);
+};
+
+
+export{
+    validateCreatePost
+}
